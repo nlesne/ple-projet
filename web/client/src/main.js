1e32d0bd07273574f101ec4a4cc436b82e6eda5c
Lucile <ldauriac@etu.u-bordeaux.fr>
2021-01-12 17:39:29 +0100

    begin part web client

--- web/client/src/main.js
+++ web/client/src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+
+Vue.use(Buefy);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
---
+++ web/client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: '',
+  props: {},
+  data() {},
+  components: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
