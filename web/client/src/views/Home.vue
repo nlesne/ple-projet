cfc7f77467d6b8b3873a8f3c1d71100534414661
Lucile <ldauriac@etu.u-bordeaux.fr>
2021-01-14 20:25:22 +0100

    part web

--- web/client/src/views/Home.vue
+++ web/client/src/views/Home.vue
@@ -9,8 +9,20 @@
       <div class="subtitle"> Hastags : </div>
       <b-button type="is-primary is-light" outlined
         v-on:click="onHastagTopK"> Top k </b-button>
+      <b-button type="is-primary is-info" outlined
+        v-on:click="onCountHastag"> Nombre d'utilisation d'un hashtag</b-button>
+      <b-button type="is-primary is-light" outlined
+        v-on:click="onUserForHashtag">
+          Liste d'utilisateur pour un hashtag</b-button>
       <div class="subtitle"> Users : </div>
-      <div class="subtitle"> influenceurs : </div>
+      <b-button type="is-primary is-info" outlined
+        v-on:click="onHastagUser"> Nombre d'Hashtag d'un utilisateur</b-button>
+      <b-button type="is-primary is-light" outlined
+        v-on:click="onTweetUser"> Nombre de Tweet d'un utilisateur</b-button>
+      <b-button type="is-primary is-info" outlined
+        v-on:click="onTweetCountry">Nombre de Tweet par pays</b-button>
+      <b-button type="is-primary is-light" outlined
+        v-on:click="onTweetLanguage"> Nombre de Tweet par langue</b-button>
     </b-sidebar>
     <div class="body">
       <div class="title">Résultat : </div>
@@ -35,7 +47,25 @@ export default {
       text.innerHTML = res;
     },
     onHastagTopK: function() {
-      this.modifyBody('Top K K !');
+      this.modifyBody('Top K !');
+    },
+    onCountHastag: function() {
+      this.modifyBody('Compte un hashtag!');
+    },
+    onUserForHashtag: function() {
+      this.modifyBody('Liste dutilisateur pour un hashtag!');
+    },
+    onHastagUser: function() {
+      this.modifyBody('Nombre d hashtag pour un utilisateur!');
+    },
+    onTweetUser: function() {
+      this.modifyBody('Nombre de tweet pour un utilisateur!');
+    },
+    onTweetCountry: function() {
+      this.modifyBody('Nombre de tweet par pays');
+    },
+    onTweetLanguage: function() {
+      this.modifyBody('Nombre de tweet par langue');
     },
   },
 };
