89f16f4b0d0b9ea1d8d016235840bbddaca24835
Lucile Dauriac <ldauriac@emi.u-bordeaux.fr>
2021-01-23 22:46:24 +0100

    top k hashtag

--- bigdata/src/main/java/hashtags/KHashtags.java
+++ bigdata/src/main/java/hashtags/KHashtags.java
@@ -1,174 +1,195 @@
 package hashtags;
 
 import parser.*;
-import parser.TweetParser.TPMapper;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class KHashtags {
 
-	public static class Hastags implements Writable {
-		
+	public static class Hashtags implements Writable {
+
 		public String name;
 		public int total;
-		
-		public Hastags() {
+
+		public Hashtags() {
 			this.name = "";
 			this.total = 0;
 		}
-		
-		public Hastags(String name, int total) {
+
+		public Hashtags(String name, int total) {
 			this.name = name;
 			this.total = total;
 		}
-		
+
 		public void readFields(DataInput in) throws IOException {
 			this.name = in.readUTF();
 			this.total = in.readInt();
 		}
-		
+
 		public void write(DataOutput out) throws IOException {
 			out.writeUTF(name);
 			out.writeInt(total);
 		}
 	}
 
-	public static class CountOneHastagCombiner extends Reducer<LongWritable, Tweet, Text, IntWritable> {
-		public void reduce(LongWritable key, Iterable<Tweet> values, Context context) throws IOException, InterruptedException {
-			for(Tweet tweet : values){
-				ArrayList<String> listHashtag = tweet.getHastags();
-				int size = listHashtag.size();
-				for(int i=0; i<size; i++){
-					String word = listHashtag.get(i);
-					context.write(new Text(word), new IntWritable(1));
-				}
+	public static class CountMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+		@Override
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+			JsonNode root = new ObjectMapper().readTree(value.toString());
+
+			if (root.has("delete")) {
+				return;
+			}
+
+			ArrayList<String> hashtags = new ArrayList<>(root.get("entities").get("hashtags").findValuesAsText("text"));
+			for (String hash : hashtags) {
+				context.write(new Text(hash), new IntWritable(1));
 			}
 		}
 	}
 
-	public static class CountAllHastagCombiner extends Reducer<Text,IntWritable,NullWritable,Hastags> {
+	public static class CountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-		public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
 			int sum = 0;
 			for (IntWritable val : values) {
 				sum += val.get();
 			}
-			Hastags hastags = new Hastags(key.toString(), sum);
-			context.write(NullWritable.get(), hastags);
+			context.write(key, new IntWritable(sum));
 		}
 	}
 
-	public static class TopKHastagCombiner extends Reducer<NullWritable,Hastags,NullWritable,Hastags> {
+	public static class TopKMapper extends Mapper<LongWritable, Text, NullWritable, Hashtags> {
 
-		private int k = 10;
-		
-		@Override
-		protected void setup(Reducer<NullWritable, Hastags, NullWritable, Hastags>.Context context)
+		private TreeMap<Integer, String> topk = new TreeMap<Integer, String>();
+    private int k = 10;
+    
+    @Override
+		protected void setup(Mapper<LongWritable, Text, NullWritable, Hashtags>.Context context)
 				throws IOException, InterruptedException {
 			this.k = context.getConfiguration().getInt("k", 10);
 		}
 
-		public void reduce(Text key, Iterable<Hastags> values,Context context) throws IOException, InterruptedException {
-			TreeMap<Integer, String> topk = new TreeMap<Integer, String>();
-			for(Hastags hash : values) {
-				topk.put(hash.total, hash.name);
-				while(topk.size() > k) {
-					topk.remove(topk.firstKey());
-				}
-			}
-			for (Map.Entry<Integer, String> v : topk.entrySet()) {
-				context.write(NullWritable.get(), new Hastags(v.getValue(), v.getKey()));
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String[] tokens = value.toString().split("\\s+");
+			int tot = Integer.valueOf(tokens[1]);
+			String name = tokens[0];
+			topk.put(tot, name);
+			
+			while (topk.size() > k)
+				topk.remove(topk.firstKey());
+
+			for(Map.Entry<Integer,String> pair : topk.entrySet()) {
+				Hashtags h = new Hashtags(pair.getValue(), pair.getKey());
+				context.write(NullWritable.get(), h);
 			}
 		}
-	} 
+	}
 
-	public static class TopKHastagReducer extends Reducer<NullWritable,Hastags,NullWritable,Hastags> {
-
-		private int k = 10;
-		
-		@Override
-		protected void setup(Reducer<NullWritable, Hastags, NullWritable, Hastags>.Context context)
+	public static class TopKReducer extends Reducer<NullWritable, Hashtags, IntWritable, Text> {
+    private int k = 10;
+    
+		protected void setup(Mapper<NullWritable, Hashtags, IntWritable, Text>.Context context)
 				throws IOException, InterruptedException {
 			this.k = context.getConfiguration().getInt("k", 10);
 		}
 
-		public void reduce(NullWritable key, Iterable<Hastags> values,Context context) throws IOException, InterruptedException {
+		@Override
+		protected void reduce(NullWritable key, Iterable<Hashtags> values, Reducer<NullWritable, Hashtags, IntWritable, Text>.Context context)
+				throws IOException, InterruptedException {
 			TreeMap<Integer, String> topk = new TreeMap<Integer, String>();
-			for(Hastags hash : values) {
-				topk.put(hash.total, hash.name);
+			for(Hashtags cp : values) {
+				//insertion de la ville
+				topk.put(cp.total, cp.name);
+				// on conserve les k plus grandes
 				while(topk.size() > k) {
 					topk.remove(topk.firstKey());
 				}
 			}
+			
+			// ecrire les k plus grandes
 			for (Map.Entry<Integer, String> v : topk.entrySet()) {
-				context.write(NullWritable.get(), new Hastags(v.getValue(), v.getKey()));
+				context.write(new IntWritable(v.getKey()), new Text(v.getValue()));
 			}
 		}
-	} 
+	}
 
-    public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		int k = 0;
-		k = Integer.parseInt(args[0]);
-		conf.setInt("k", k);
+	public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
 
-    Job job = Job.getInstance(conf, "TwitterProject");
+		//////////////Job 1
+		Job job = Job.getInstance(conf);
 		job.setNumReduceTasks(1);
 
-		
 		job.setInputFormatClass(TextInputFormat.class);
 		TextInputFormat.addInputPath(job, new Path(args[1]));
-		
-		//Mapper
-		job.setMapperClass(TPMapper.class);
-    job.setMapOutputKeyClass(LongWritable.class);
-		job.setMapOutputValueClass(Tweet.class);
-		
+
 		job.setJarByClass(KHashtags.class);
-		//Combiner
-		job.setCombinerClass(CountOneHastagCombiner.class);
-		job.setCombinerClass(CountAllHastagCombiner.class);
-		job.setCombinerClass(TopKHastagCombiner.class);
-		//Reducer
-		job.setReducerClass(TopKHastagReducer.class);
-    job.setOutputKeyClass(Text.class);
+		// Mapper
+		job.setMapperClass(CountMapper.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		// Reducer
+		job.setReducerClass(CountReducer.class);
+		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 
-    job.setOutputFormatClass(TextOutputFormat.class);			
-		try {
-			FileInputFormat.addInputPath(job, new Path(args[0]));
-			FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		  }
-		  catch (Exception e) {
-			System.out.println(" bad arguments, waiting for 2 arguments [inputURI] [outputURI]");
-		  }
-		  int res = -1;
-		  res = job.waitForCompletion(true) ? 0 : 1;
-		  System.exit(res);
-  }
+		Path outputPath = new Path("FirstMapper");
+		FileOutputFormat.setOutputPath(job, outputPath);
+		job.waitForCompletion(true);
+
+
+		//////////Job 2
+    Configuration conf2 = new Configuration();
+    
+    int k = 0;
+		k = Integer.parseInt(args[0]);
+    conf2.setInt("k", k);
+    
+		Job job2 = Job.getInstance(conf2);
+		job2.setNumReduceTasks(1);
+
+		TextInputFormat.addInputPath(job2, outputPath) ;
+
+		job2.setJarByClass(KHashtags.class);
+
+		job2.setMapperClass(TopKMapper.class);
+		job2.setMapOutputKeyClass(NullWritable.class);
+		job2.setMapOutputValueClass(Hashtags.class);
+
+		// Reducer
+		job2.setReducerClass(TopKReducer.class);
+		job2.setOutputKeyClass(IntWritable.class);
+		job2.setOutputValueClass(Text.class);
+
+		FileOutputFormat.setOutputPath(job2, new Path(args[2]));
+		System.exit(job2.waitForCompletion(true) ? 0 : 1);
+	}
 }
+
