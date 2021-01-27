738b14da90cc92c52856487f978ffdd8a8684b1b
Nathan Lesné <nathan.lesne@etu.u-bordeaux.fr>
2021-01-27 22:44:18 +0100

    Fixed typo in server.js

--- web/server/server.js
+++ web/server/server.js
@@ -6,7 +6,7 @@ const hbase = require('hbase');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({origin: [serverConfig.client_url]}));
+app.use(cors({origin: [config.client_url]}));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/hashtags', require('./routes/hashtags'));
