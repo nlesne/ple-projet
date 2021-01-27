7a3aa1de159d81fe5bcba553faf75f28e05dcb85
Lucile <ldauriac@etu.u-bordeaux.fr>
2021-01-27 16:23:47 +0100

    view web

--- web/client/src/views/Home.vue
+++ web/client/src/views/Home.vue
@@ -6,30 +6,64 @@
       open
       :can-cancel="false">
       <div class="title">Tri : </div>
+      <b-field label="Jour:">
+        <b-input v-model="day"></b-input>
+      </b-field>
       <div class="subtitle"> Hastags : </div>
-      <b-button type="is-primary is-light" outlined
+
+      <b-button type="is-primary is-info" outlined
         v-on:click="onHastagTopK"> Top k </b-button>
-      <b-button type="is-primary is-info" outlined
-        v-on:click="onCountHastag"> Nombre d'utilisation d'un hashtag</b-button>
+
       <b-button type="is-primary is-light" outlined
-        v-on:click="onUserForHashtag">
-          Liste d'utilisateur pour un hashtag</b-button>
+        v-on:click="onHastagTopK"> Top k par jour</b-button>
+
+      <b-field label="Nombre d'utilisation d'un hashtag:">
+        <b-input v-model="countHashtag"></b-input>
+      </b-field>
+       <b-button type="is-primary is-info" outlined
+       v-on:click="onCountHastag"> valide</b-button>
+
+      <b-field label="Liste d'utilisateur pour un hashtag:">
+        <b-input v-model="usersForHashtag"></b-input>
+      </b-field>
+      <b-button type="is-primary is-light" outlined
+        v-on:click="onUserForHashtag"> valide</b-button>
+
       <div class="subtitle"> Users : </div>
-      <b-button type="is-primary is-info" outlined
-        v-on:click="onHastagUser"> Nombre d'Hashtag d'un utilisateur</b-button>
-      <b-button type="is-primary is-light" outlined
-        v-on:click="onTweetUser"> Nombre de Tweet d'un utilisateur</b-button>
+
       <b-button type="is-primary is-info" outlined
         v-on:click="onTweetCountry">Nombre de Tweet par pays</b-button>
+
       <b-button type="is-primary is-light" outlined
         v-on:click="onTweetLanguage"> Nombre de Tweet par langue</b-button>
+
+      <b-field label="Nombre d'Hashtag d'un utilisateur:">
+        <b-input v-model="hashtagsForUser"></b-input>
+      </b-field>
+      <b-button type="is-primary is-info" outlined
+        v-on:click="onHastagUser"> valide</b-button>
+
+      <b-field label="Nombre de Tweet d'un utilisateur:">
+        <b-input v-model="tweetForUser"></b-input>
+      </b-field>
+      <b-button type="is-primary is-light" outlined
+        v-on:click="onTweetUser">valide </b-button>
+
     </b-sidebar>
     <div class="body">
       <div class="title">Résultat : </div>
-      <ul><li v-for="ele in resultat" :key="ele">
-        <div v-if="val"> {{ele.name}} : {{ele.val}} </div>
-        <div v-else>  {{ele.name}} </div>
-      </li></ul>
+      <div v-if="begin">
+        <div v-if="val">
+          <ul><li v-for="(name, val) in resultat" :key="name">
+            <div> {{name}} : {{val}} </div>
+          </li></ul>
+        </div>
+        <div v-else>
+          <ul><li v-for="name in resultat" :key="name">
+            <div> {{name}}</div>
+          </li></ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -40,7 +74,13 @@ export default {
   props: {},
   data() {
     return {
-      val: true,
+      day: 1,
+      countHashtag: '',
+      usersForHashtag: '',
+      hashtagsForUser: '',
+      tweetForUser: '',
+      val: false,
+      begin: false,
       resultat: {
         type: Object,
       },
@@ -48,34 +88,76 @@ export default {
   },
   components: {},
   methods: {
-    modifyBody: function(res) {
-      console.log(res);
-      const body = document.querySelector('#resultat');
-      body.innerHTML = '';
-      const text = document.createElement('div');
-      body.appendChild(text);
-      text.innerHTML = res;
+    onHastagTop: function() {
+      this.begin = true;
+      this.val = true;
     },
-    onHastagTopK: function() {
-      this.modifyBody('Top K !');
+    onHastagTopKDay: function() {
+      this.begin = true;
+      this.val = true;
+
+      const param = {
+        day: this.day,
+      };
+      console.log(param);
     },
     onCountHastag: function() {
-      this.modifyBody('Compte un hashtag!');
+      this.begin = true;
+      this.val = true;
+
+      const param = {
+        day: this.day,
+        val: this.countHashtag,
+      };
+      console.log(param);
     },
     onUserForHashtag: function() {
-      this.modifyBody('Liste dutilisateur pour un hashtag!');
+      this.begin = true;
+      this.val = false;
+
+      const param = {
+        day: this.day,
+        val: this.usersForHashtag,
+      };
+      console.log(param);
     },
     onHastagUser: function() {
-      this.modifyBody('Nombre d hashtag pour un utilisateur!');
+      this.begin = true;
+      this.val = false;
+
+      const param = {
+        day: this.day,
+        val: this.hashtagsForUser,
+      };
+      console.log(param);
     },
     onTweetUser: function() {
-      this.modifyBody('Nombre de tweet pour un utilisateur!');
+      this.begin = true;
+      this.val = true;
+
+      const param = {
+        day: this.day,
+        val: this.tweetForUser,
+      };
+      console.log(param);
     },
     onTweetCountry: function() {
-      this.modifyBody('Nombre de tweet par pays');
+      this.begin = true;
+      this.val = true;
+
+      const param = {
+        day: this.day,
+      };
+      console.log(param);
     },
     onTweetLanguage: function() {
-      this.modifyBody('Nombre de tweet par langue');
+      this.begin = true;
+      this.val = true;
+
+      const param = {
+        day: this.day,
+      };
+      console.log(param);
     },
   },
 };
@@ -84,5 +166,9 @@ export default {
 <style scoped>
 .body{
   margin-left: 270px;
+}
+li {
+  list-style-type: square;
+  margin-left: 50px;
 }
 </style>
